e06e7794d0319d732d8fa5e49e4b257e4af1f2a1
zapppppp <163814616+z4phkiel@users.noreply.github.com>
2025-06-02 16:02:31 +0800

    Add files via upload

---
+++ QueryResto/src/queryresto/order.java
@@ -0,0 +1,20 @@
+package queryresto;
+
+
+public class order {
+    private String foodOrder;
+    private String diningOption;
+
+    order(String foodOrder, String diningOption) {
+        this.foodOrder = foodOrder;
+        this.diningOption = diningOption;
+    }
+
+    @Override 
+    public String toString() {
+        return "Order{" +
+                "foodOrder='" + foodOrder + '\'' +
+                ", diningOption='" + diningOption + '\'' +
+                '}';
+    }
+}
